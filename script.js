b13787bfe5bf1c48f82d267b72b89478df0be64f
hallo-sys <zackvkv@gmail.com>
2025-06-17 13:17:08 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+  function showSupport() {
+      document.getElementById('popup').c
+lassList.remove('hidden');
+    }
+
+    function closePopup() {
+    document.getElementById('popup').classList.add('hidden');}
